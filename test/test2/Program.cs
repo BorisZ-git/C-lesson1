fce9730f460cbbb9abcd5157fa2c136f5bf1a95e
BorisZ-git <ribos@bk.ru>
2017-02-26 01:05:34 +0300

    task2\\records variables: height and weight\\ set cm to m in variable height

---
+++ test/test2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+//Boris Zn
+/*
+ * Ввести вес и рост человека.
+ *  Расчитать и вывести индекс массы тела(ИМТ) по формуле  I=m/(h*h);
+ *   где m ­масса тела в килограммах, h ­ рост в метрах
+ *     *Интерпритировать показания ИМТ​.
+ */
+namespace test2
+{
+    class Program
+    {
+        static void Main()
+        {
+            //records variables(записываем переменные)
+            Console.WriteLine("Добрый день! Программа поможет Вам вывести вашу ИМТ");
+            Console.WriteLine("Введите ваш рост в см");
+            string height = Console.ReadLine();
+            Console.WriteLine("Ваш вес в килограммах");
+            string weight = Console.ReadLine();
+            // set cm to m(приводим см к метрам)
+            // m = 100 cm 
+            double h = (Convert.ToInt32(height)) / 100;
+
+        }
+    }
+}
---
+++ test/test2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("test2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("test2")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f45f4ab0-f4eb-470b-b2e5-7f310883a6a3")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
