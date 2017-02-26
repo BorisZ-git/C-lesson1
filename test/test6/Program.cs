59e61dc5ef54cdf025c548973569e3edae944419
BorisZ-git <ribos@bk.ru>
2017-02-26 13:53:36 +0300

    add method Print\\add method Pause

--- test/test6/Program.cs
+++ test/test6/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+/* Boris Z
+ * Создайте класс с методами, которые могут пригодиться в вашей учебе(Print, Pause). 
+*/
 namespace test6
 {
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
+        }
+        public void Print (string msg, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(msg);
         }
     }
 }
